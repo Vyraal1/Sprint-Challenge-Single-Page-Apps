defbd138cf67fdd1363ef359b7a3e746b8117db2
Vyraal1 <kevin.afable@gmail.com>
2019-09-20 13:52:48 -0400

    moving stupid request to parent component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,34 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://rickandmortyapi.com/api/character/")
-      .then(res => {
-        setCharacters(res.data.results);
-      })
-      .catch(e => console.log(e));
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
-  console.log(characters);
-
+export default function CharacterList(props) {
   return (
     <section className="character-list">
-      {characters.map(({ name, gender, status }) => {
-        return (
-          <CharacterCard
-            key={name}
-            name={name}
-            gender={gender}
-            status={status}
-          />
-        );
-      })}
+      {props.characters.map(character => (
+        <CharacterCard
+          key={character.name}
+          name={character.name}
+          gender={character.gender}
+          status={character.status}
+        />
+      ))}
     </section>
   );
 }
